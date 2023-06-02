1bf62777e003eefdf67f0bd2d99e9840bfc7872e
rshelar21 <89639649+rshelar21@users.noreply.github.com>
2023-06-02 20:17:40 +0530

    backend-url

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : "https://amazon-backend-8s1m.onrender.com"
+    baseURL : "https://amazon-backend-i9mn.onrender.com"
 })
 
 export default instance;
